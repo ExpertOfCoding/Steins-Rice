5b7cd74d8c153d30584d7e050e9f9ea2e45ae8dd
ExpertOfCoding <141582636+ExpertOfCoding@users.noreply.github.com>
2026-09-11 18:24:09 +0000

    $HOME/themes

---
+++ themes/matugen/colors-hyprland.lua
@@ -0,0 +1,21 @@
+return {
+    -- Ana Vurgu Renkleri (Border, Aktif Bütünlükler)
+    primary         = "rgba(c9bfffee)",
+    on_primary      = "rgba(2e009cee)",
+    
+    -- İkincil ve Üçüncül Vurgular (Gradient border yapmak istersen)
+    secondary       = "rgba(d3bfe6ee)",
+    tertiary        = "rgba(e3b8ebee)",
+
+    -- Arka Plan ve Yüzeyler (Blur, Opacity, İnaktif Border)
+    background      = "rgba(14121cee)",
+    surface         = "rgba(14121cee)",
+    surface_variant = "rgba(484551ee)",
+    
+    -- Metin / Ön Plan Renkleri
+    foreground      = "rgba(e5e0efee)",
+    on_surface      = "rgba(e5e0efee)",
+
+    -- Hata / Uyanış Renkleri (Gerekirse)
+    error           = "rgba(ffb4abee)",
+}
